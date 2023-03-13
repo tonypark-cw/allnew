8995b4fb731a4d9d58d0f62f3c3e20db44011676
tonypark-cw <qkrcksdna@naver.com>
2023-03-13 11:44:11 +0900

    merging

--- c/age.c
+++ c/age.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 int main(){
   int i ;
@@ -6,3 +7,13 @@ int main(){
   printf("%d 살이네요!\n", i);
   return 0;
 }
+=======
+#include <stdio.h>
+int main(){
+  int i ;
+  printf("나이를 입력하세요 : ");
+  scanf("%d", &i);
+  printf("%d 살이네요!\n", i);
+  return 0;
+}
+>>>>>>> 77f07ff (sync 0313)
