7246d02e7212e4b63b1a9269e8973a967e4dfe48
doongle <qkrcksdna@naver.com>
2023-03-16 14:37:32 +0900

    header add

--- c/gcc/age.c
+++ c/gcc/age.c
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
 #include <stdio.h>
-int main(){
-  int i ;
-  printf("나이를 입력하세요 : ");
-  scanf("%d", &i);
-  printf("%d 살이네요!\n", i);
-  return 0;
+#include "myheader.h"
+
+void main() {
+  printf("My age is %d \n", AGE);
 }
-=======
-#include <stdio.h>
-int main(){
-  int i ;
-  printf("나이를 입력하세요 : ");
-  scanf("%d", &i);
-  printf("%d 살이네요!\n", i);
-  return 0;
-}
->>>>>>> 77f07ff (sync 0313)
